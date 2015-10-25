2820a15d2c896336752e4d25ca1809757d5bd165
Shonda Kiser <kisersi@users.noreply.github.com>
2015-10-25 09:35:52 -0400

    Update cachematrix.R
    Made changes to placement of brackets and comments

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,7 @@ makeCacheMatrix <- function(x = matrix()) {
         inv_x <- NULL
         set <- function(y) { ## creates the matrix in the working environment
                 x <<- y
-                inv_x <<- NULL ## Use '<<-' to assign a value to an object in an environment
+                inv_x <<- NULL ## The use of '<<-' is to assign a value to an object in an environment
                                 # different from the current environment.
         }
         get <- function() x ## Gets the value of the matrix
@@ -26,12 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv_x <- x$getinverse() ## This will attempt to get the inverse of the matrix if it is stored in the cache
-        if (!is.null(inv_x)) { ## return inverted matrix from the cache if it exists
+        if (!is.null(inv_x)) { ## returns the inverted matrix from the cache if it exists
                 message("Please hold...getting cached inverse matrix") ## Displays the message
                 return(inv_x) ## Displays the Matrix in console
         } else {
-                inv_x <- solve(x$get()) ## Matrix will be created since it does not exist
-                x$setinverse(inv_x) ## set the inverted matrix in cache
+                inv_x <- solve(x$get()) ## A matrix will be created since it does not exist
+                x$setinverse(inv_x) ## sets the inverted matrix in cache
                 return(inv_x) ## Displays the Matrix in the console
         }
 }
